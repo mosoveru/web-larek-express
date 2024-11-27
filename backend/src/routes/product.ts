49fa3dd2b72c04db8a5c6ed6af0bdd1298068db5
Mosoveru <mosoveru@gmail.com>
2024-11-27 15:31:11 +0300

    fix: add product schema
    Added a product schema for the '/product' route to validate requests using Joi library.

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,11 +1,29 @@
 import express from 'express';
-import path from 'path';
+import {
+  Joi, celebrate, Segments, errors,
+} from 'celebrate';
 import { getAllProducts, createProduct } from '../controllers/product';
+
+const productSchema = Joi.object({
+  title: Joi.string().required().min(2).max(30),
+  image: Joi.object({
+    fileName: Joi.string().required(),
+    originalName: Joi.string().required(),
+  }),
+  category: Joi.string().required(),
+  description: Joi.string(),
+  price: Joi.valid(Joi.number(), null),
+});
+
+const productRouteValidator = celebrate({
+  [Segments.BODY]: productSchema,
+});
 
 const router = express.Router();
 
 router.use(express.json());
-router.use(express.static(path.join(__dirname, '../public')));
+router.use(productRouteValidator);
+router.use(errors());
 router.get('/product', getAllProducts);
 router.post('/product', createProduct);
 
