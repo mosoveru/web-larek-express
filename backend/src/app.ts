61cb632305dc43bbe05f8cf7b7caeff4fcc15840
Mosoveru <mosoveru@gmail.com>
2024-11-28 08:51:19 +0300

    feat: implement logging
    Implemented request and error logging in .log files.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import productRouter from './routes/product';
 import orderRouter from './routes/order';
 import defaultRouter from './routes/index';
 import handleErrors from './middlewares/error-handler';
+import { requestLogger, errorLogger } from './middlewares/logger';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,10 +16,12 @@ const DB_ADDRESS = process.env.DB_ADDRESS || 'mongodb://127.0.0.1:27017';
 mongoose.connect(DB_ADDRESS);
 
 app.use(cors());
+app.use(requestLogger);
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(productRouter);
 app.use(orderRouter);
 app.use(defaultRouter);
+app.use(errorLogger);
 app.use(handleErrors);
 
 app.listen(PORT, () => {
---
+++ backend/src/middlewares/logger.ts
@@ -0,0 +1,16 @@
+import winston from 'winston';
+import expressWinston from 'express-winston';
+
+export const requestLogger = expressWinston.logger({
+  transports: [
+    new winston.transports.File({ filename: 'request.log' }),
+  ],
+  format: winston.format.json({ space: 2 }),
+});
+
+export const errorLogger = expressWinston.errorLogger({
+  transports: [
+    new winston.transports.File({ filename: 'error.log' }),
+  ],
+  format: winston.format.json({ space: 2 }),
+});
